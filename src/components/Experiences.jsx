1a80fe84751f91d6badf2862ebdaece72b99cfdb
Kmasetya <dwinataardi41@gmail.com>
2025-11-26 17:30:41 +0700

    merubah gambar

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import data from "../data/data.json";
+import fosti from "../assets/fosti.webp";
 
 const Experiences = () => {
   return (
     <div className="bg-white rounded-3xl p-8 shadow-sm mb-6 border border-gray-100">
-      <div className="bg-white rounded-3xl p-8 shadow-sm mb-6 border border-gray-100">
-        <h2 className="text-2xl font-bold text-gray-800 mb-6"> Experiences</h2>
-        {/* 2. TEKS (Mengambil SISA ruang penuh) */}
-        <div className="flex-1 text-center md:text-left">
-          {/* 3. BARIS JUDUL (Memaksa H3 dan SPAN ke ujung) */}
-          <div className="flex justify-between items-center">
-            <h3 className="font-bold text-gray-800 text-lg">
-              {data.skills[1].name}
-            </h3>
-            <span className="bg-blue-300 px-3 py-1 rounded-full backdrop-blur-sm flex-none">
-              Main
-            </span>
+      <h2 className="text-2xl font-bold text-gray-800 mb-8">Experiences</h2>
+      <div className="flex items-start gap-6">
+        <div className="flex-none flex flex-col items-center">
+          <div className="w-25 h-25 bg-white rounded-full flex items-center justify-center shadow-sm z-10">
+            <img src={fosti} alt="" className="w-20 h-15 flex-shrink-0" />
           </div>
-          <h1 className="font-normal text-gray-500 text-lg">
-            {data.skills[1].years_of_experience} years experience
-          </h1>
+        </div>
+        <div className="flex-1 border border-gray-200 rounded-xl p-6 hover:shadow-md transition-shadow duration-300">
+          <div className="flex justify-between items-start mb-1">
+            <h3 className="font-bold text-gray-900 text-lg">
+              Fosti (UI/UX Designer)
+            </h3>
+          </div>
         </div>
       </div>
     </div>
